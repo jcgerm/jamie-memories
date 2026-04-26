3ad821dcf49c3248337c067883773ceb6c9e7c00
Jeremy Fried <jcgerm@gmail.com>
2026-04-26 14:54:50 -0400

    Scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
 import SubmitPage from './pages/SubmitPage'
 import AdminPage from './pages/AdminPage'
 import GalleryPage from './pages/GalleryPage'
@@ -11,6 +17,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         <Route path="/" element={<Navigate to="/memories" replace />} />
         <Route path="/memories" element={<GalleryPage />} />
